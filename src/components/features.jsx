5a87e5d94a84033b1b166ad752458c8930c12a65
Inga Kozlowska <inga@snowschool.pl>
2022-10-14 21:33:54 +0200

    Initial commit

---
+++ src/components/features.jsx
@@ -0,0 +1,15 @@
+export const Features = (props) => {
+  return (
+    <div id='cennik' className='text-center'>
+      <div className='container'>
+        <div className='col-md-10 col-md-offset-1 section-title'>
+          <h2>Cennik</h2>
+        </div>
+        </div>
+        <div>   <h5>table here</h5></div> 
+       
+      
+      
+    </div>
+  )
+}
